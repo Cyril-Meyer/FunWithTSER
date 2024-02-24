d4ee3e9eff4089c9b28e9675822007cf967d5943
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-02-24 19:32:50 +0100

    update data prepare script

--- data_prepare.py
+++ data_prepare.py
@@ -5,6 +5,15 @@ from sktime.datasets import load_from_tsfile
 
 DATA_PATH = './Monash_UEA_UCR_Regression_Archive/'
 
+def convert_to_npy(data_):
+    data_ = np.array(data_)
+    data_shape2 = len(data_[0,0])
+    data = np.zeros((data_.shape[0], data_.shape[1], data_shape2), dtype=np.float64)
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            data[i, j, :] = np.array(data_[i, j])
+    return data
+
 for data in ['BIDMC32HR', 'BIDMC32RR', 'BIDMC32SpO2']:
     '''
     If you have a UnicodeDecodeError: 'utf-8' :
@@ -13,7 +22,7 @@ for data in ['BIDMC32HR', 'BIDMC32RR', 'BIDMC32SpO2']:
     '''
     train_x, train_y = load_from_tsfile(os.path.join(DATA_PATH, f"{data}/{data}_TRAIN.ts"))
     test_x,  test_y  = load_from_tsfile(os.path.join(DATA_PATH, f"{data}/{data}_TEST.ts"))
-    np.save(f'dataset/{data}_train_x',train_x)
+    np.save(f'dataset/{data}_train_x',convert_to_npy(train_x))
     np.save(f'dataset/{data}_train_y',train_y)
-    np.save(f'dataset/{data}_test_x', test_x)
+    np.save(f'dataset/{data}_test_x', convert_to_npy(test_x))
     np.save(f'dataset/{data}_test_y', test_y)
