e30d7589843139f5d079ee7aae5cdf357841496b
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-02-25 17:51:10 +0100

    update view function in evaluate module

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,18 @@
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.metrics
 
 
 def view(y, pred, figsize=(18, 4)):
+    pred, y = np.array(pred), np.array(y)
+    sort = y.argsort(axis=None).reshape(y.shape)
+    pred = pred.ravel()[sort]
+    y = y.ravel()[sort]
+
     plt.figure(figsize=figsize)
-    y, pred = zip(*sorted(list(zip(y, pred))))
-    plt.scatter(x=[i for i in range(len(y))], y=y)
-    plt.scatter(x=[i for i in range(len(y))], y=pred)
+    plt.scatter(x=[i for i in range(len(y))], y=y,    color='#00ff00ff')
+    plt.scatter(x=[i for i in range(len(y))], y=pred, color='#ff000030')
     plt.show()
     
 def mse(y, pred):
